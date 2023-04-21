40674b0ab498474f5bb8f48963b643f74194e35b
Rodney <123950191+rodneyd05@users.noreply.github.com>
2023-04-21 22:03:39 +0800

    Added SubjectActivity

---
+++ app/src/main/java/com/thisisit/studentapp/SubjectActivity.kt
@@ -0,0 +1,11 @@
+package com.thisisit.studentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubjectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subject)
+    }
+}
